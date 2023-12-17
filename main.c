35968b2f35950c281d9ddaae023f66f3fcdfaf25
Asmae2001 <himdiasmaebr@gmail.com>
2023-12-17 15:26:07 -0800

    task

--- main.c
+++ main.c
@@ -1,45 +1,95 @@
 #include "monty.h"
-bus_t bus = {NULL, NULL, NULL, 0};
+
 /**
-* main - monty code interpreter
-* @argc: number of arguments
-* @argv: monty file location
-* Return: 0 on success
-*/
-int main(int argc, char *argv[])
+ * main - main function
+ * @argc: argument counts
+ * @argv: arguments passed
+ * Return: success
+ */
+int main(int argc, char **argv)
 {
-	char *content;
 	FILE *file;
-	size_t size = 0;
-	ssize_t read_line = 1;
-	stack_t *stack = NULL;
-	unsigned int counter = 0;
+	char *buff = NULL, *string;
+	size_t s = 0;
+	unsigned int linenum = 1;
+	stack_t *stk = NULL;
 
 	if (argc != 2)
 	{
-		fprintf(stderr, "USAGE: monty file\n");
+		printf("USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
 	file = fopen(argv[1], "r");
-	bus.file = file;
-	if (!file)
+	if (file == NULL)
 	{
-		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		printf("Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
-	while (read_line > 0)
+	while (getline(&buff, &s, file) != -1)
 	{
-		content = NULL;
-		read_line = getline(&content, &size, file);
-		bus.content = content;
-		counter++;
-		if (read_line > 0)
+		if (*buff != '\n')
 		{
-			execute(content, &stack, counter, file);
+			string = strtok(buff, "\n");
+			_tokenizer(string, &stk, linenum);
 		}
-		free(content);
+		linenum++;
 	}
-	free_stack(stack);
 	fclose(file);
-return (0);
+	free(buff);
+	if (stk != NULL)
+		free_stk(&stk, linenum);
+	return (EXIT_SUCCESS);
+}
+
+/**
+ * _tokenizer - function to tokenize strings and commands
+ * @string: string to be tokenized
+ * @stk: pointer to the stack
+ * @linenum: line numbers
+ * Return: void
+ */
+void _tokenizer(char *string, stack_t **stk, unsigned int linenum)
+{
+	char *token;
+	char *tokens;
+
+	token = strtok(string, " ");
+	if (token == NULL || *token == ' ' || *token == '\n' || *token == '#')
+		return;
+	if (strcmp(token, "push") == 0)
+	{
+		tokens = token;
+		token = strtok(NULL, " ");
+		if (!check_digit(token))
+		{
+			printf("L%d: usage: push integer\n", linenum);
+			free_stk(stk, linenum);
+			exit(EXIT_FAILURE);
+		}
+		holder = atoi(token);
+		_ops(tokens, stk, linenum);
+	}
+	else
+		_ops(token, stk, linenum);
+}
+
+/**
+ * check_digit - checks if string is a number
+ * @token: string to check
+ * Return: 1 if number, 0 if not
+ */
+int check_digit(char *token)
+{
+	if (token == NULL)
+		return (0);
+	if (*token == '-')
+		token++;
+	while (*token != '\0')
+	{
+		if (!isdigit(*token))
+			return (0);
+		token++;
+	}
+	token++;
+	return (1);
 }
